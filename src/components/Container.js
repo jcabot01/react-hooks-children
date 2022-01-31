387675693c6e6090c77791648b45b8b0b2523a2c
JonCabot <jfc0053@gmail.com>
2022-01-30 20:12:42 -0600

    completed assignment

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 function Container({
-  header,
-  children,
+  header, //this is used in 2 ways "Example header" from the first Container call, then again by the picture Container call
+  children, //all of the duplicate "This is an example" <div>s & turtle <img>s are children, they get called in
   textPosition = "", // possible values: left, right, center
   direction = "horizontal", // possible values: vertical, horizontal
   contentPosition = "center", // possible values: left, right, center
 }) {
   return (
-    <div className={`container ${textPosition}`}>
+    <div className={`container ${textPosition}`}> {/**/}
       {header ? <h2>{header}</h2> : null}
 
       <div className={`container-children ${direction} ${contentPosition}`}>
